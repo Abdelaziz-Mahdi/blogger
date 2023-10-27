e5b95a82c3a04a9ff2dba63c721e7e03320282bf
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-27 17:44:56 +0200

    Fix linters

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  #test methods
+  # test methods
   it "update_likes_counter should update post's likes_counter" do
-    user = User.create(name: "User 1")
-    post = Post.create(title: "Post 1", author: user)
+    user = User.create(name: 'User 1')
+    post = Post.create(title: 'Post 1', author: user)
     expect(post.likes_counter).to eq(0)
-    Like.create(user: user, post: post)
-    Like.create(user: user, post: post)
-    Like.create(user: user, post: post)
+    Like.create(user:, post:)
+    Like.create(user:, post:)
+    Like.create(user:, post:)
     expect(post.likes_counter).to eq(3)
   end
 end
