25ea7d647b1fca1c20beb24bb6e27d347e9a94f8
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-10-27 17:42:51 +0200

    Add unit test for like method

---
+++ spec/models/like_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe Like, type: :model do
+  #test methods
+  it "update_likes_counter should update post's likes_counter" do
+    user = User.create(name: "User 1")
+    post = Post.create(title: "Post 1", author: user)
+    expect(post.likes_counter).to eq(0)
+    Like.create(user: user, post: post)
+    Like.create(user: user, post: post)
+    Like.create(user: user, post: post)
+    expect(post.likes_counter).to eq(3)
+  end
+end
