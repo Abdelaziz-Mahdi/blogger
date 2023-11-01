422ba16a3b605572f78c661b351c4f368db365f8
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-01 16:28:07 +0200

    Test users controller If correct template rendered

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,18 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  # If response status was correct
+  # If response status was correct.
   describe 'GET /users' do
-    it 'index works! have_http_status(200)' do
+    it 'Action index works! have_http_status(200)' do
       get users_path
       expect(response).to have_http_status(200)
     end
   end
 
   describe 'GET /users/:id' do
-    it 'show works! have_http_status(200)' do
+    it 'Action show works! have_http_status(200)' do
       get user_path(1)
       expect(response).to have_http_status(200)
+    end
+  end
+
+  #If a correct template was rendered.
+  describe 'GET /users' do
+    it 'Action index works! render_template(:index)' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /users/:id' do
+    it 'Action show works! render_template(:show)' do
+      get user_path(1)
+      expect(response).to render_template(:show)
     end
   end
 end
