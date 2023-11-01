3b40a99e0e1686fd747655cb241972286705c6ec
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-01 16:28:17 +0200

    Test users controller If correct template rendered

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -11,8 +11,23 @@ RSpec.describe 'Posts', type: :request do
 
   describe 'GET /users/:user_id/posts/:id' do
     it 'Action show works! have_http_status(200)' do
-      get user_posts_path(1, 1)
+      get user_post_path(1, 1)
       expect(response).to have_http_status(200)
+    end
+  end
+
+  # If a correct template was rendered
+  describe 'GET /users/:user_id/posts' do
+    it 'Action index works! render_template(:index)' do
+      get user_posts_path(1)
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'Action show works! render_template(:show)' do
+      get user_post_path(1, 1)
+      expect(response).to render_template(:show)
     end
   end
 end
