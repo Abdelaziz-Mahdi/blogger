26f27005df487593480f269371031898a67df44a
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-01 15:13:40 +0200

    Test posts controller If response status correct

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  # If response status was correct
+  describe 'GET /users/:user_id/posts' do
+    it 'Action index works! have_http_status(200)' do
+      get user_posts_path(1)
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'Action show works! have_http_status(200)' do
+      get user_posts_path(1, 1)
+      expect(response).to have_http_status(200)
+    end
+  end
+end
