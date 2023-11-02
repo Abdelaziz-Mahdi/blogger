766bd2a8ce480955a159fc735d8e65b11177fa98
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-02 12:47:01 +0200

    Add user, post, and comment associations to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,14 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.order(created_at: :desc).page(params[:page])
+  end
 
-  def show; end
+  def show
+    @post = Post.includes(:author, :likes, comments: [:user] ).find_by_id(params[:id])
+    @post_comments = @post.comments
+    @post_like = @post.likes[0]
+    @post_author = @post.author
+    @post_comment_author = @post_comments.map(&:user).flatten
+  end
 end
