a1663656f80c96cb83360e4cff2e3d22539eeefb
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-11-09 16:22:32 +0200

    Define admin roles in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable
+         :recoverable, :rememberable, :validatable
+
   has_many :posts, dependent: :destroy, foreign_key: :author_id
   has_many :comments, dependent: :destroy, foreign_key: :user_id
   has_many :likes, dependent: :destroy, foreign_key: :user_id
 
   validates :name, presence: true
   validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
+
+  # Define roles
+  enum role: { user: 'user', admin: 'admin' }
+
+  # Ability rules for CancanCan
+  def ability
+    @ability ||= Ability.new(self)
+  end
+
+  delegate :can?, :cannot?, to: :ability
 
   def three_most_recent_posts
     posts.order(created_at: :desc).limit(3)
